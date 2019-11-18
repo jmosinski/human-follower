08db4d1f29be8d0f9e27439ec3558cfa89b41bb6
Mateusz Moskała <mmoskala07@gmail.com>
2019-11-18 23:45:16 +0100

    New version of core code

--- core2/main.c
+++ core2/main.c
@@ -16,12 +16,12 @@ using namespace hFramework;
 // Const
 #define ENGINE_FORWARD -1
 #define ENGINE_BACKWARD 1
-#define STEERING_LEFT 1
-#define STEERING_RIGHT -1
+#define STEERING_LEFT -1
+#define STEERING_RIGHT 1
 
-#define ENGINE_P_GAIN = 500
-#define STEERING_P_GAIN = 1000
-#define STEERING_MAX_ANGLE_RAD = 45 * 3.14/180.0 // 45 degrees
+#define ENGINE_P_GAIN 1500.0
+#define STEERING_P_GAIN 1000.0
+#define STEERING_MAX_ANGLE_RAD 45.0 * 3.14/180.0 // 45 degrees
 
 // Blocked steering turn
 #define BLOCKED_NONE 0
@@ -38,6 +38,7 @@ geometry_msgs::Twist cmd_vel;
 int straightEncoderVal;
 int leftMaxEncoderVal;
 int rightMaxEncoderVal;
+bool DEBUG = false;
 
 // ROS definitions
 ros::NodeHandle nh;
@@ -49,8 +50,15 @@ ros::Subscriber<geometry_msgs::Twist> *cmd_vel_sub;
 
 void CmdVelSubscriber(const geometry_msgs::Twist& msg)
 {
-        cmd_vel = msg;
-        cmd_vel_pub->publish(&cmd_vel);
+        // OLD
+        // cmd_vel = msg;
+
+        // NEW
+        cmd_vel.linear.x = msg.linear.x;
+        cmd_vel.angular.z = msg.angular.z;
+
+        cmd_vel.angular.x++;
+        cmd_vel.angular.y++;
 }
 void initBatteryPublisher()
 {
@@ -66,7 +74,7 @@ void initCmdVel()
 }
 
 // Body
-bool key_pressed(hSensors::Lego_Touch &device, int threshold=10)
+bool key_pressed(hSensors::Lego_Touch &device, int threshold=3)
 {
         static int endurance = 0;
         if (device.isPressed())
@@ -80,69 +88,28 @@ bool key_pressed(hSensors::Lego_Touch &device, int threshold=10)
                 return false;
 }
 
-int getAckermannEncoderReferenceValues()
-{
-    // Get data
-    turn(STEERING_RIGHT, 500);
-    sleep(2000*1000); // Wait minimum 2s until key press
-    while(!key_pressed(sensor));
-    rightMaxEncoderVal = steering.getEncoderCnt();
-
-    turn(STEERING_LEFT, 500);
-    sleep(2000*1000); // Wait minimum 2s until key press
-    while(!key_pressed(sensor));
-    leftMaxEncoderVal = steering.getEncoderCnt();
-
-    // Calculate variables
-    straightEncoderVal = (rightMaxEncoderVal + leftMaxEncoderVal) / 2
-}
-
-void turn_controller(int desiredAngle)
-{
-    // Get actual encoder value
-    actualEncoderVal = steering.getEncoderCnt();
-
-    // Calculate relative values
-    relativeActualEncoderVal = actualEncoderVal - straightEncoderVal;
-    relativeLeftMaxEncoderVal = leftMaxEncoderVal - straightEncoderVal;
-    relativeRightMaxEncoderVal = rightMaxEncoderVal - straightEncoderVal;
-
-    // Calculate actual angle
-    if (abs(relativeActualEncoderVal - relativeLeftMaxEncoderVal) < abs(relativeActualEncoderVal - relativeRightMaxEncoderVal))
-        actualAngle = (relativeActualEncoderVal / relativeLeftMaxEncoderVal) * STEERING_MAX_ANGLE_RAD * (1) // LEFT SIDE
-    else
-        actualAngle = (relativeActualEncoderVal / relativeRightMaxEncoderVal) * STEERING_MAX_ANGLE_RAD * (-1) // RIGHT SIDE
-
-    // Calculate steering value and execute steering function
-    steering_value = (desiredAngle - actualAngle) * STEERING_P_GAIN;
-    if (steering_value > 0)
-        turn(STEERING_LEFT, steering_value);
-    else
-        turn(STEERING_RIGHT, steering_value);
-}
-
 void turn(int turn_side, int power)
 {
         static int blocked_turn = BLOCKED_NONE;
         static int can_block = true;
 
         /// INFO
-        /*
-        switch(blocked_turn)
+        if (DEBUG)
         {
-                case BLOCKED_NONE:
-                        break;
-                case BLOCKED_TURN_LEFT:
-                        printf("Blocked left turn \r\n");
-                        break;
-                case BLOCKED_TURN_RIGHT:
-                        printf("Blocked right turn \r\n");
-                        break;
+                switch(blocked_turn)
+                {
+                        case BLOCKED_NONE:
+                                break;
+                        case BLOCKED_TURN_LEFT:
+                                printf("Blocked left turn \r\n");
+                                break;
+                        case BLOCKED_TURN_RIGHT:
+                                printf("Blocked right turn \r\n");
+                                break;
+                }
         }
-        */
-
         /// BLOCKING TURN
-        bool max_turn_reached = key_pressed(sensor);//sensor.isPressed();
+        bool max_turn_reached = key_pressed(sensor);
         if(max_turn_reached)
         {
                 if(turn_side == STEERING_LEFT && can_block)
@@ -153,7 +120,8 @@ void turn(int turn_side, int power)
                 {
                         blocked_turn = STEERING_RIGHT;
                 }
-                //printf("Max_turn_reached \r\n");
+                if (DEBUG)
+                        printf("Max_turn_reached \r\n");
                 can_block = false;
         }
         else
@@ -165,59 +133,149 @@ void turn(int turn_side, int power)
         /// STEERING
         if(blocked_turn != BLOCKED_NONE && turn_side == blocked_turn)
         {
-                //printf("That action is blocked! \r\n");
+                if (DEBUG)
+                        printf("That action is blocked! %d \r\n", blocked_turn);
                 steering.setPower(0);
         }
         else
         {
-                steering.setPower(turn_side*power);
-                //if (abs(power) > 0)
-                //      printf("Steering works! \r\n");
+                steering.setPower(turn_side*abs(power));
+                if (DEBUG)
+                {
+                        if (turn_side == STEERING_LEFT)
+                                printf("Steering left! \r\n");
+                        else if (turn_side == STEERING_RIGHT)
+                                printf("Steering right! \r\n");
+                }
         }
 }
 
-void move(int power)
+float turn_controller(float desiredAngle)
+{
+    // Get actual encoder value
+    int actualEncoderVal = steering.getEncoderCnt();
+
+    // Calculate relative values
+    int relativeActualEncoderVal = actualEncoderVal - straightEncoderVal;
+    int relativeLeftMaxEncoderVal = leftMaxEncoderVal - straightEncoderVal;
+    int relativeRightMaxEncoderVal = rightMaxEncoderVal - straightEncoderVal;
+    float actualAngle;
+
+    // Calculate actual angle
+    if (abs(relativeActualEncoderVal - relativeLeftMaxEncoderVal) < abs(relativeActualEncoderVal - relativeRightMaxEncoderVal))
+        actualAngle = (float(relativeActualEncoderVal) / relativeLeftMaxEncoderVal) * STEERING_MAX_ANGLE_RAD * (1.0); // LEFT SIDE
+    else
+        actualAngle = (float(relativeActualEncoderVal) / relativeRightMaxEncoderVal) * STEERING_MAX_ANGLE_RAD * (-1.0); // RIGHT SIDE
+
+    // Calculate steering value and execute steering function
+    int steering_value = (desiredAngle - actualAngle) * STEERING_P_GAIN;
+    if (DEBUG)
+    {
+        printf("Actual angle %f \r\n", actualAngle);
+        printf("Steering value %d \r\n", steering_value);
+    }
+    if (steering_value > 0)
+        turn(STEERING_LEFT, steering_value);
+    else
+        turn(STEERING_RIGHT, steering_value);
+
+    return actualAngle;
+}
+
+void move(float power)
 {
         engine.setPower(ENGINE_FORWARD*ENGINE_P_GAIN*power);
 }
 
+
+void getAckermannEncoderReferenceValues()
+{
+    // Get data
+    turn(STEERING_RIGHT, 400);
+    sys.delay(2000); // Wait minimum 2s until key press
+	while(!key_pressed(sensor))
+		turn(STEERING_RIGHT, 400);
+	turn(STEERING_RIGHT, 400);
+    rightMaxEncoderVal = steering.getEncoderCnt();
+
+    turn(STEERING_LEFT, 400);
+    sys.delay(2000); // Wait minimum 2s until key press
+	while(!key_pressed(sensor))
+		turn(STEERING_LEFT, 400);
+	turn(STEERING_LEFT, 400);
+    leftMaxEncoderVal = steering.getEncoderCnt();
+
+    // Calculate variables
+    straightEncoderVal = (rightMaxEncoderVal + leftMaxEncoderVal) / 2.0;
+
+    // INFO
+    if (DEBUG)
+    {
+            printf("Left %d \r\n", leftMaxEncoderVal);
+            printf("Right %d \r\n", rightMaxEncoderVal);
+            printf("Straight %d \r\n", straightEncoderVal);
+            sys.delay(2000);
+    }
+
+    // Position to straight wheels
+    while(abs(turn_controller(0)) > 0.1)
+        sys.delay(10);
+    steering.setPower(0);
+}
+
 void hMain()
 {
+        // 1. Increase baud rate to 57600
+        // 2. Look what is happening in subscribers
+        // 3. Maybe moving publishing to main loop have already fixed problem
+        // 4. Try to turn battery publisher
+        // 5. Add ros rate
+        // 6. Try to measure times in main loop and subscribers frequency
         Serial.init(9600, Parity::None, StopBits::One);
         nh.getHardware()->initWithDevice(&Serial);
         nh.initNode();
         initBatteryPublisher();
         initCmdVel();
-        getAckermannEncoderReferenceValues()
+        sys.delay(2000);
+        getAckermannEncoderReferenceValues();
 
         uint32_t t = sys.getRefTime();
-
-        // int power;
-        // int turn_side;
+        int loop_cnt = 0;
 
         while(true)
         {
+                // DEBUG
+                if (DEBUG)
+                {
+                    int turn_side = 0;
+                    int power = 0;
+                    if(hBtn2.isPressed())
+                    {
+                            turn_side = STEERING_LEFT;
+                            power = 1000;
+                    }
+                    if(hBtn1.isPressed())
+                    {
+                            turn_side = STEERING_RIGHT;
+                            power = 1000;
+                    }
+                    turn(turn_side, power);
+                }
+
+                // MAIN LOOP
                 nh.spinOnce();
-
-                // turn_side = 0;
-                // power = 0;
-                // if(hBtn2.isPressed())
-                // {
-                //         turn_side = STEERING_LEFT;
-                //         power = 250;
-                // }
-                // if(hBtn1.isPressed())
-                // {
-                //         turn_side = STEERING_RIGHT;
-                //         power = 250;
-                // }
-                // turn(turn_side, power);
-
                 turn_controller(cmd_vel.angular.z);
                 move(cmd_vel.linear.x);
-
-                battery.voltage = sys.getSupplyVoltage();
-                battery_pub->publish(&battery);
-                sys.delaySync(t, 20);
+                if((loop_cnt % 100) == 0)
+                {
+                        battery.voltage = sys.getSupplyVoltage();
+                        battery_pub->publish(&battery);
+                }
+                if ((loop_cnt % 10) == 0)
+                {
+                        cmd_vel_pub->publish(&cmd_vel);
+                }
+                sys.delaySync(t, 10);
+                loop_cnt++;
         }
 }
